cda06f4fda9b6dc595c11e36bfc015ca8cf5eda5
halittugra <48262224+halittugra@users.noreply.github.com>
2020-01-21 18:19:49 +0300

    Create ScrappingLinks.java

---
+++ ScrappingLinks.java
@@ -0,0 +1,24 @@
+import java.net.URL;
+import java.util.Scanner;
+import java.io.IOException;
+
+public class ScrappingLinks {
+    
+    public static void main(String[] args) throws IOException{
+        
+        String address = "https://codeforces.com/";
+        URL pageLocation = new URL(address);
+        Scanner scn = new Scanner(pageLocation.openStream());
+        
+        while(in.hasNext()){
+            String line = scn.next();
+            if(line.contains("href=\"http://") || line.contains("href=\"https://")){
+                int basla = line.indexOf("\"");
+                int bitir= line.lastIndexOf("\"");
+                System.out.println(line.substring(basla + 1,bitir));
+            }
+        
+        }
+    }
+    
+}
